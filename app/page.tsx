0f7fe9fcbd0d54f0186fec31e2586f5ca7049a3a
David Serret <david.mcs1111@gmail.com>
2026-05-14 12:41:30 +0200

    debug 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { redirect } from "next/navigation"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/lib/auth"
+
 export const runtime = "nodejs"
 
-export default function Test() {
-  return <div>FUNCIONA</div>
+// 🔥 ESTA ES LA CLAVE REAL
+export const dynamic = "force-dynamic"
+
+export default async function RootPage() {
+  const session = await getServerSession(authOptions)
+
+  if (session) {
+    redirect("/dashboard")
+  } else {
+    redirect("/login")
+  }
 }
